c121d8bec7647e3b7557b42a6b1af3ece7f40475
Rahul Kumar <rahul.kumar@ajmerainfotech.com>
2024-04-04 19:23:03 +0530

    change

--- QR_SCAN_PROFILE_LINK/Controllers/HomeController.cs
+++ QR_SCAN_PROFILE_LINK/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using QR_SCAN_PROFILE_LINK.Models;
 using QRCoder;
-using System.Diagnostics;
 
 namespace QR_SCAN_PROFILE_LINK.Controllers
 {
@@ -20,16 +19,17 @@ namespace QR_SCAN_PROFILE_LINK.Controllers
             // Simulate the generation of unique scan file name.
             var scanFileName = Guid.NewGuid().ToString();
 
-            // Add scanFileName in QR_Verification_Table, the ProfileId and Status not be populated now, they will be added when the user scans the QR.
+            // Add the unique ScanFileName in QrVerification Table
+            // The ProfileId and Status will not be populated now, they will be added when the user scans the QR.
             qrVerifications.Add(new QrVerification() { ScanFileName = scanFileName });
 
-            // Create the QR Code using the scanFileName.
-            // The Desktop Applicatoin will create the QR code using scan id.
+            // Generation of the QR Code with the scanFileName.
+            // The Desktop Application will create the QR code using the unique scan file name.
 
-            // Create a QRCodeData Object for the given payload
-            QRCodeData qrCodeData = qrGenerator.CreateQrCode(scanFileName, QRCodeGenerator.ECCLevel.Q);
+            // Create a QRCodeData Object for the given unique scan filename.
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(scanFileName, QRCodeGenerator.ECCLevel.Q);  // QRCodeGenerator.ECCLevel.Q will decide the Accuracy level of the QR.
 
-            // Create a byte array that will store the QR code image
+            // Create a byte array that will store the QR code image.
             PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
             int pixelSize = 20;
             var qrCodeImage = qrCode.GetGraphic(pixelSize);
@@ -38,42 +38,51 @@ namespace QR_SCAN_PROFILE_LINK.Controllers
             string base64Image = Convert.ToBase64String(qrCodeImage);
 
             ViewBag.QRCodeImage = $"data:image/png;base64,{base64Image}";
-            ViewBag.ScanFileName = scanFileName;    
-            return View();
-        }
+            ViewBag.ScanFileName = scanFileName;
 
-        public IActionResult Privacy()
-        {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
         /// <summary>
-        /// This API will take scanId and ProfileId and will Map both.
+        /// This API will map scanId and ProfileId and will Map both.
         /// </summary>
         /// <param name="ScanFileName"></param>
         /// <returns></returns>
-        //[HttpPost("map-scan-to-profile")]
         public IActionResult MapScanToProfile([FromQuery] string scanFileName)
         {
+            // Find the record with given scan file name.
             var scanToMap = qrVerifications.Find(opts => opts.ScanFileName == scanFileName);
+
+            // Map the scan file name with given Profile and mark the status to true.
             scanToMap!.ProfileId = Guid.NewGuid().ToString();
             scanToMap.Status = true;
+
+            // Until the Desktop/Web App Confirms the scan and profile mapping, I am showing a temporary view.
+            // The temporary view will show that the mapping is successful and it is waiting for Desktop/Web App to read the mapping.
             ViewBag.ScanFileName = scanToMap.ScanFileName;
             ViewBag.ProfileId = scanToMap.ProfileId;
+
             return View();
         }
-        //[HttpGet("ReadScanMapping")]
+
+        /// <summary>
+        /// This Function will be called from Web/Desktop App at some interval.
+        /// We will check if the give scanFileName is mapped to profile.
+        /// </summary>
+        /// <param name="scanFileName"></param>
+        /// <returns></returns>
         public IActionResult ReadScanMapping([FromQuery] string scanFileName)
         {
+            // Find The Record With Given scanFileName in QrVerification Table.
             var mappedScan = qrVerifications.Find(opts => opts.ScanFileName == scanFileName);
 
-            var isScanMappedSuccessfully = !string.IsNullOrEmpty(mappedScan!.ProfileId) && mappedScan.Status == true;
+            // Check if the ProfileId and Status has been added to the table for given scanFileName.
+            // If the ProfileId and Status is found then it means that user(profile) has scanned the QR the scan is mapped to his profile.
+            var isScanMappedSuccessfully = false;
+            if (mappedScan != null)
+            {
+                isScanMappedSuccessfully = !string.IsNullOrEmpty(mappedScan!.ProfileId) && mappedScan.Status == true;
+            }
             return Ok(isScanMappedSuccessfully);
         }
 
